cc25f96d5f3066117a2bc5bd672e0b12b96ff2f7
IGNACIO RUIZ CASTILLO <ignacio.ruiz@ctag.com>
2025-11-14 08:46:03 +0000

    test formatted

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,8 +6,7 @@ import pytest
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 @pytest.fixture
